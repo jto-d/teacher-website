4543e6ca57332820e85a0a6386edb7df3aae56b4
Evantl17 <82623080+Evantl17@users.noreply.github.com>
2021-12-03 13:19:59 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 var pictureNum = 1;
 const pictureOne = "boogaard.jpeg";
-const pictureTwo = "new-arrow.png";
+const pictureTwo = "3678016F-1A6A-45B0-86C4-CD2724FF92E6.jpg";
+const pictureThree = "F02370A4-A6AD-432F-BD0B-7DFEB05A21E9.jpg";
+const pictureFour = "67389675-863F-4CF0-BF45-85DFC931946B.jpg"
+const pictureFive = "574EAE32-532A-4E0A-B9D7-969958E9C68C.jpg";
 var numbers =['indexOne', 'indexTwo', 'indexThree', 'indexFour', 'indexFive'];
-var pictures = [pictureOne, pictureTwo];
+var pictures = [pictureOne, pictureTwo, pictureThree, pictureFour, pictureFive];
 
 function rightArrow(){
     console.log('right arrow');
-    if(pictureNum < 1){
+    if(pictureNum < pictures.length-1){
         pictureNum++;
 
     }
@@ -14,8 +17,18 @@ function rightArrow(){
         pictureNum = 0;
     }
     document.getElementById('mainPicture').src = pictures[pictureNum];
-    document.getElementById(numbers[pictureNum]).style.color = "white";
-    document.getElementById(numbers[pictureNum]).style.color = "black";
+    console.log(pictureNum);
+    
+    for(var i = 0;i<pictures.length;i++){
+        if(i==pictureNum){
+            document.getElementById(numbers[pictureNum]).style.color = "red";
+        }
+        else{
+            document.getElementById(numbers[i]).style.color = "blue";
+        }
+        
+    }
+   
 }
 
 function leftArrow(){
