9dc48602827fe206fe44aa688642b54ef9a3ff1a
Jack <90474780+jto-d@users.noreply.github.com>
2021-12-03 13:29:20 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var pictureNum = 1;
-const pictureOne = "boogaard.jpeg";
-const pictureTwo = "3678016F-1A6A-45B0-86C4-CD2724FF92E6.jpg";
-const pictureThree = "F02370A4-A6AD-432F-BD0B-7DFEB05A21E9.jpg";
-const pictureFour = "67389675-863F-4CF0-BF45-85DFC931946B.jpg"
-const pictureFive = "574EAE32-532A-4E0A-B9D7-969958E9C68C.jpg";
+const pictureOne = "home-pictures/boogaard.jpeg";
+const pictureTwo = "home-pictures/3678016F-1A6A-45B0-86C4-CD2724FF92E6.jpg";
+const pictureThree = "home-pictures/F02370A4-A6AD-432F-BD0B-7DFEB05A21E9.jpg";
+const pictureFour = "home-pictures/67389675-863F-4CF0-BF45-85DFC931946B.jpg"
+const pictureFive = "home-pictures/574EAE32-532A-4E0A-B9D7-969958E9C68C.jpg";
 var numbers =['indexOne', 'indexTwo', 'indexThree', 'indexFour', 'indexFive'];
 var pictures = [pictureOne, pictureTwo, pictureThree, pictureFour, pictureFive];
 
