007102c085b37f68652285623a5e1b13314dbff3
Jack <90474780+jto-d@users.noreply.github.com>
2021-12-06 00:26:47 -0500

    Add files via upload

--- calendar.js
+++ calendar.js
@@ -1,57 +1,78 @@
-const date = new Date();
+const date = new Date()
 
 const leapYears = [2024,2028,2032,2036,2040,2044]
 if(leapYears.includes(date.getFullYear)) {
     months[1][1]=29
 }
 
-const months = [
-    ["January", 31],
-    ["February", 28],
-    ["March", 31],
-    ["April", 30],
-    ["May", 31],
-    ["June", 30],
-    ["July", 31],
-    ["August", 31],
-    ["September", 30],
-    ["October", 31],
-    ["November", 30],
-    ["December", 31]
-]
+const calendar = () => {
+    
+    
+    const months = [
+        ["January", 31],
+        ["February", 28],
+        ["March", 31],
+        ["April", 30],
+        ["May", 31],
+        ["June", 30],
+        ["July", 31],
+        ["August", 31],
+        ["September", 30],
+        ["October", 31],
+        ["November", 30],
+        ["December", 31]
+    ]
 
-const currentMonth = date.getMonth()
-document.querySelector(".date h1").innerHTML = months[currentMonth][0]
+    const currentMonth = date.getMonth()
+    document.querySelector(".date h1").innerHTML = months[currentMonth][0]
 
-document.querySelector(".date p").innerHTML = date.toDateString();
+    document.querySelector(".date p").innerHTML = date.toDateString()
 
-const calDays = document.querySelector(".days")
+    const calDays = document.querySelector(".days")
 
-const initDate = new Date(date.getFullYear(), date.getMonth(), 0).getDay()-1
-const finDate = new Date(date.getFullYear(), date.getMonth(), months[currentMonth][1]).getDay()-1
+    const initDate = new Date(date.getFullYear(), date.getMonth(), 0).getDay()-1
+    const finDate = new Date(date.getFullYear(), date.getMonth(), months[currentMonth][1]).getDay()-1
 
 
-var days = [];
-for(var day = 1; day<=months[currentMonth][1]; day++) {
-    if(day == date.getDate())
-        days.push(`<div class="today">${day}</div>`)
+    var days = []
+    for(var day = 1; day<=months[currentMonth][1]; day++) {
+        if(day == date.getDate())
+            days.push(`<div class="today">${day}</div>`)
+        else
+            days.push(`<div>${day}</div>`)
+    }
+
+    if(currentMonth == 0)
+        var lastMonth = 11
     else
-        days.push(`<div>${day}</div>`)
+        var lastMonth = currentMonth-1
+
+
+    var prevDays = []
+    for(var day = initDate; day>=0; day--) {
+        prevDays.push(`<div class="prev-date">${months[lastMonth][1]-day}</div>`)
+    }
+
+    var nextDays = []
+    for(var day = 1; day <= 42 - (prevDays.length + days.length); day++){
+        nextDays.push(`<div class="next-date">${day}</div>`)
+    }
+
+
+    calDays.innerHTML = prevDays.join('') + days.join('') + nextDays.join('')
 }
 
-
-var prevDays = [];
-for(var day = initDate; day>=0; day--) {
-    prevDays.push(`<div class="prev-date">${months[currentMonth-1][1]-day}</div>`)
-}
-
-var nextDays = [];
-for(var day = 1; day <= 42 - (prevDays.length + days.length); day++){
-    nextDays.push(`<div class="next-date">${day}</div>`)
-}
-
-
-calDays.innerHTML = prevDays.join('') + days.join('') + nextDays.join('');
-
+document.querySelector(".prev").addEventListener("click", () => {
+    date.setMonth(date.getMonth() - 1)
+    calendar()
+  })
+  
+  document.querySelector(".next").addEventListener("click", () => {
+    date.setMonth(date.getMonth() + 1)
+    console.log(date)
+    calendar()
+  })
+  
+calendar()
 
 
