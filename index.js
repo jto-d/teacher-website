51c70f2b9d9595512f0915d51c74f26c818eb7be
Evantl17 <82623080+Evantl17@users.noreply.github.com>
2021-12-06 08:59:12 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -6,38 +6,22 @@ const pictureFour = "home-pictures/67389675-863F-4CF0-BF45-85DFC931946B.jpg"
 const pictureFive = "home-pictures/574EAE32-532A-4E0A-B9D7-969958E9C68C.jpg";
 var numbers =['indexOne', 'indexTwo', 'indexThree', 'indexFour', 'indexFive'];
 var pictures = [pictureOne, pictureTwo, pictureThree, pictureFour, pictureFive];
-
-function rightArrow(){
-    console.log('right arrow');
-    if(pictureNum < pictures.length-1){
-        pictureNum++;
-
-    }
-    else{
-        pictureNum = 0;
-    }
-    document.getElementById('mainPicture').src = pictures[pictureNum];
-    console.log(pictureNum);
-    
-    for(var i = 0;i<pictures.length;i++){
-        if(i==pictureNum){
-            document.getElementById(numbers[pictureNum]).style.color = "red";
-        }
-        else{
-            document.getElementById(numbers[i]).style.color = "blue";
-        }
-        
-    }
-   
+var Interval;
+var seconds;
+var miliseconds;
+var Interval;
+window.onload = function() { 
+    Interval = setInterval(startTimer, 5000);
 }
 
-function leftArrow(){
-    console.log(pictureNum);
-    if(pictureNum > 0){
-        pictureNum--;
-    }
-    else{
-        pictureNum = pictures.length-1;
-    }
-    document.getElementById('mainPicture').src = pictures[pictureNum];
+
+function startTimer(){
+        console.log("run");
+        document.getElementById('mainPicture').src = pictures[pictureNum];
+        if(pictureNum > pictures.length-2){
+            pictureNum = 0;
+        }
+        else{
+            pictureNum++;
+        }
 }
