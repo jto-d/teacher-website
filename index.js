c89874422049c9f287b95a4d22448b48ba0ae7fa
Evantl17 <82623080+Evantl17@users.noreply.github.com>
2021-12-07 14:43:46 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var pictureNum = 1;
 const pictureOne = "home-pictures/boogaard.jpeg";
 const pictureTwo = "home-pictures/3678016F-1A6A-45B0-86C4-CD2724FF92E6.jpg";
 const pictureThree = "home-pictures/F02370A4-A6AD-432F-BD0B-7DFEB05A21E9.jpg";
-const pictureFour = "home-pictures/67389675-863F-4CF0-BF45-85DFC931946B.jpg"
+const pictureFour = "home-pictures/E-Nwyl_XMAYD5W-.jpeg"
 const pictureFive = "home-pictures/574EAE32-532A-4E0A-B9D7-969958E9C68C.jpg";
 var numbers =['indexOne', 'indexTwo', 'indexThree', 'indexFour', 'indexFive'];
 var pictures = [pictureOne, pictureTwo, pictureThree, pictureFour, pictureFive];
@@ -26,7 +26,6 @@ function startTimer(){
 
             }
         }
-        
         document.getElementById(numbers[pictureNum]).style.color = "aqua";
         if(pictureNum > pictures.length-2){
             pictureNum = 0;
@@ -37,17 +36,19 @@ function startTimer(){
 }
 
 function numClick(x){
-    console.log(x);
     clearInterval(Interval);
     Interval = setInterval(startTimer, 5000);
-    pictureNum = x-1;
+    if(x==1){
+        pictureNum = 0;
+    }
+    else{
+        pictureNum = x-1;
+    }
     for(var i = 0;i<numbers.length;i++){
         if(i!=pictureNum){
             document.getElementById(numbers[i]).style.color = "white";
-
         }
     }
-    
     document.getElementById(numbers[pictureNum]).style.color = "aqua";
     document.getElementById('mainPicture').src = pictures[x-1];
 }
