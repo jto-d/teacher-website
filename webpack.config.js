204e4557abc0ce004ecc0317c78172a8e58e2ba0
Jack <90474780+jto-d@users.noreply.github.com>
2022-03-08 19:21:35 -0500

    Add files via upload

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,34 @@
-const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    mode: 'development',
-    entry: {
-        app: './src/index.js',
-        login_app: './src/login-page.js',
-
-    },
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    },
-    watch: true,
-}
+  entry: {
+    calendar: './src/js/calendar.js',
+    admin: './src/js/admin.js',
+    login: './src/js/login.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'build', 'target'),
+    publicPath: '/',
+    filename: '[name].bundle.js',
+    chunkFilename: '[id].bundle_[chunkhash].js',
+    sourceMapFilename: '[file].map'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'calendar.html',
+      template: 'src/calendar.html',
+      chunks: ['calendar']
+    }),
+    new HtmlWebpackPlugin({
+      filename: 'admin.html',
+      template: 'src/admin.html',
+      chunks: ['admin']
+    }),
+    new HtmlWebpackPlugin({
+        filename: 'login.html',
+        template: 'src/login.html',
+        chunks: ['login']
+      })
+  ]
+  
+};
